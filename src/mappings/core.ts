ebe1ba63d20f583c30dd267edb7776b6b6f19551
Sergey <itsib.su@gmail.com>
2021-10-11 13:26:59 +0700

    Fixed deploy script and some minor fixes

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -196,15 +196,21 @@ export function handleTransfer(event: Transfer): void {
 
   if (from.toHexString() != ADDRESS_ZERO && from.toHexString() != pair.id) {
     let fromUserLiquidityPosition = createLiquidityPosition(event.address, from)
-    fromUserLiquidityPosition.liquidityTokenBalance = convertTokenToDecimal(pairContract.balanceOf(from), BI_18)
-    fromUserLiquidityPosition.save()
+    let result = pairContract.try_balanceOf(from)
+    if (!result.reverted) {
+      fromUserLiquidityPosition.liquidityTokenBalance = convertTokenToDecimal(result.value, BI_18)
+      fromUserLiquidityPosition.save()
+    }
     createLiquiditySnapshot(fromUserLiquidityPosition, event)
   }
 
   if (event.params.to.toHexString() != ADDRESS_ZERO && to.toHexString() != pair.id) {
     let toUserLiquidityPosition = createLiquidityPosition(event.address, to)
-    toUserLiquidityPosition.liquidityTokenBalance = convertTokenToDecimal(pairContract.balanceOf(to), BI_18)
-    toUserLiquidityPosition.save()
+    let result = pairContract.try_balanceOf(to)
+    if (!result.reverted) {
+      toUserLiquidityPosition.liquidityTokenBalance = convertTokenToDecimal(result.value, BI_18)
+      toUserLiquidityPosition.save()
+    }
     createLiquiditySnapshot(toUserLiquidityPosition, event)
   }
 
